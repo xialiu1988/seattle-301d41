efc03a00085c9ba539c2b9b978548f25d4de0822
xialiu1988 <liuxia198896@gmail.com>
2018-11-26 13:51:10 -0800

    finished the app.js in arrow-function

--- 00-prep/lab/1-ES6-practice/arrow-functions/app.js
+++ 00-prep/lab/1-ES6-practice/arrow-functions/app.js
@@ -168,55 +168,54 @@ console.log(message('Allie'));
 // };
 
 
-let student=(name,age,hometown)=>({
-  name:name,
-  hometown:hometown,
-  age:age,
-});
-
-let joe=student('Joe', 'Schmoe', 100);
+class Student{
+  constructor(name,hometown,age){
+    this.name=name;
+    this.hometown=hometown;
+    this.age=age;
+  }
+}
+const joe = new Student('Joe', 'Schmoe', 100);
 
 // TODO: Uncomment the following line of code to see the output in the browser console
 // Note that the arrow function will cause this code to break!
 console.log(joe);
 
 
-student.prototype.greeting = function() {
-  return `Hi, my name is ${this.name}`;
-};
+Student.prototype.greeting=()=> `Hi, my name is ${joe.name}`;
 
 // TODO: Uncomment the following line of code to see the output in the browser console
 // Note that the arrow function will cause this method to break!
 console.log(joe.greeting());
 
 
-this.courseName = function() {
-  return 'This student is enrolled in Code 301.';
-};
+Student.courseName = ()=> 'This student is enrolled in Code 301.';
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-console.log(this.courseName());
+console.log(Student.courseName());
 
 
 
 // STEP 11
 // How do arrow functions affect constructor functions?
+
+//where constructor should convert from function to class
 Student.prototype.scope = function() {
   console.log(this);
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(joe.scope());
+console.log(joe.scope());
 
 Student.prototype.scopeArrow = () => console.log(this);
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(joe.scopeArrow());
+console.log(joe.scopeArrow());
 
 // TODO: Write a COMMENT below to answer the following questions.
 // 1. What is "this" when joe.scope() is invoked?
-//
+// undefined
 // 2. What is "this" when joe.scopeArrow() is invoked?
-//
+// it popped up window object
 // 3. Explain why "this" is different when an arrow function is used.
-//
+// arrow functions  can't be generators
